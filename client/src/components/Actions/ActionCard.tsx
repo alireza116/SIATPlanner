54b46682ed46e870ca30b22bd35324abb969d1c2
alireza116 <alireza116@gmail.com>
2025-02-04 22:11:37 -0800

    removed unwanted imports.

--- client/src/components/Actions/ActionCard.tsx
+++ client/src/components/Actions/ActionCard.tsx
@@ -6,8 +6,6 @@ import {
   TextField,
   Button,
   Box,
-  Chip,
-  CardContent,
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
